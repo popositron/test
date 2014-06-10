9415252a6a0be40bf925197f37bda2d8252a49ed
Chaton <ptoudoux@gmail.com>
2014-06-10 14:46:34 +0100

    qwe

---
+++ GuildBrowser Tests/GuildBrowser_Tests.m
@@ -0,0 +1,31 @@
+//
+//  GuildBrowser_Tests.m
+//  GuildBrowser Tests
+//
+//  Created by Chaton on 10/06/2014.
+//  Copyright (c) 2014 Charlie Fulton. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GuildBrowser_Tests : XCTestCase
+
+@end
+
+@implementation GuildBrowser_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+
+@end
